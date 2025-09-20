08c25a1cd27259f2f5e257495f63f1bcfd3f43f3
Jason Ross <jasonross6064@gmail.com>
2025-09-20 07:23:02 -0700

    update

--- part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -5,6 +5,12 @@ public class CountingToHundred {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        int input = Integer.valueOf(scanner.nextLine());
+        
+        for(int i = 100; i >= input; input++) {
+            System.out.println(input);
+        }
+        
     }
 }
--- part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -6,6 +6,14 @@ public class FromWhereToWhere {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Where to?");
+        int input = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("Where form?");
+        int input2 = Integer.valueOf(scanner.nextLine());
+
+        for (int i = input2; i <= input; i++) {
+            System.out.println(i);
+        }
     }
 }
