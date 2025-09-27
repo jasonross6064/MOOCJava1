9698bd073788fbd2a910c1670c437587d32ff581
Jason Ross <jasonross6064@gmail.com>
2025-09-27 03:43:28 -0700

    update

--- part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -5,6 +5,18 @@ public class SumOfASequenceTheSequel {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        int sum = 0;
+        
+        System.out.println("First number?");
+        int first = Integer.valueOf(scanner.nextLine());
+        System.out.println("Second number?");
+        int second = Integer.valueOf(scanner.nextLine());
+        
+        for (int i = first; i <= second; i++) {
+            sum = sum + i;
+        }
+        System.out.println("The sum is: " + sum);
+        
     }
 }
--- part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -4,15 +4,45 @@ import java.util.Scanner;
 public class RepeatingBreakingAndRemembering {
 
     public static void main(String[] args) {
-        
+
         // This exercise is worth five exercise points, and it is 
         // gradually extended part by part.
-        
         // If you want, you can send this exercise to the server
         // when it's just partially done. In that case the server will complain about 
         // the parts you haven't done, but you'll get points for the finished parts.
-        
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        int inputs = 0;
+        int evens = 0;
+        int odds = 0;
+        
+        while (true) {
+            System.out.println("Give numbers:");
+            int input = Integer.valueOf(scanner.nextLine());
+
+            if (input == -1) {
+                break;
+            }
+            if (input % 2 == 0) {
+                evens++;
+            }
+            
+            if (input % 2 == 1) {
+                odds++;
+            }
+            
+            sum = sum + input;
+            inputs++;
+        }
+        
+        double average = sum / (inputs * 1.0);
+                
+        System.out.println("Thx! Bye!");
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + inputs);
+        System.out.println("Average: " + average);
+        System.out.println("Even: " + evens);
+        System.out.println("Odd: " + odds);
     }
 }
