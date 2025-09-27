8bca8c42f6324ac453337c683c96284cd15c0387
Jason Ross <jasonross6064@gmail.com>
2025-09-27 06:01:21 -0700

    update

--- part02-Part02_28.Word/src/main/java/Word.java
+++ part02-Part02_28.Word/src/main/java/Word.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 
 public class Word {
 
+    public static String word() {
+        return "Hello there";
+    }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
