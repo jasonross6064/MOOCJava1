a79083e5545204292696e16379353230bed9aa6a
Jason Ross <jasonross6064@gmail.com>
2025-10-25 04:09:44 -0700

    update

--- part02-Part02_33.StarSign/src/main/java/StarSign.java
+++ part02-Part02_33.StarSign/src/main/java/StarSign.java
@@ -3,11 +3,9 @@ public class StarSign {
 
     public static void main(String[] args) {
 
-        
         //The tests are not checking the main, so you can modify it freely.
         //NB: If the tests don't seem to pass, you should try the methods here
         //in the main to make sure they print the correct shapes!
-        
         printStars(3);
         System.out.println("\n---");  // printing --- between the shapes
         printSquare(4);
@@ -19,18 +17,36 @@ public class StarSign {
     }
 
     public static void printStars(int number) {
-        // first part of the exercise
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
     }
 
     public static void printSquare(int size) {
-        // second part of the exercise
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 
     public static void printRectangle(int width, int height) {
-        // third part of the exercise
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 
     public static void printTriangle(int size) {
-        // fourth part of the exercise
+        for (int i = 0; i <= size; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
