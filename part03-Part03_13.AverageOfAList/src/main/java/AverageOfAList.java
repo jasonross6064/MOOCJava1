f42ba23d05ef6e96e9e827ee6016457124707aa1
Jason Ross <jasonross6064@gmail.com>
2025-11-11 06:41:58 -0800

    update

--- part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
+++ part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
@@ -12,5 +12,26 @@ public class AverageOfAList {
         // Then it computes the average of the numbers on the list
         // and prints it.
         
+        ArrayList<Integer> numbers = new ArrayList<>();
+        
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+            numbers.add(input);
+        }
+        
+        int sum = 0;
+        int counter = 0;
+        
+        for (int number: numbers) {
+            sum = sum + number;
+            counter++;
+        }
+        
+        double average = sum / (counter * 1.0);
+        
+        System.out.println("Average: " + average);
     }
 }
--- part03-Part03_15.PrintInRange/src/main/java/PrintInRange.java
+++ part03-Part03_15.PrintInRange/src/main/java/PrintInRange.java
@@ -7,4 +7,12 @@ public class PrintInRange {
         // Try your method here
     }
     
+    public static void printNumbersInRange(ArrayList<Integer> numbers, int lowerLine, int upperLine) {
+        for (int number: numbers) {
+            if (number >= lowerLine && number <= upperLine) {
+                System.out.println(number);
+            }
+        }
+    }
+    
 }
