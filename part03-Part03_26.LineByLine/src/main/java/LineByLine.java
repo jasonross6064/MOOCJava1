d0d200aca324ba37009896d249e275047bcfe84d
Jason Ross <jasonross6064@gmail.com>
2025-11-18 03:59:41 -0800

    update

--- part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -6,6 +6,19 @@ public class LineByLine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] splitter = input.split(" ");
+
+            for (int i = 0; i < splitter.length; i++) {
+                System.out.println(splitter[i]);
+            }
+        }
 
     }
 }
--- part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -5,7 +5,24 @@ public class AVClub {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            
+            String[] splitter = input.split(" ");
+            
+            for (int i = 0; i < splitter.length; i++) {
+                if (splitter[i].contains("av")) {
+                    System.out.println(splitter[i]);
+                }
+            }
+            
+        }
+        
+        
     }
 }
