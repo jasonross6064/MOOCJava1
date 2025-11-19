a9dea9ad736ff2abf7b6d6026eb043fd8ce3fcab
Jason Ross <jasonross6064@gmail.com>
2025-11-18 19:33:13 -0800

    update

--- part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -4,8 +4,27 @@ import java.util.Scanner;
 public class AgeOfTheOldest {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
+        int oldest = 0;
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] breaker = input.split(",");
+
+            if (Integer.valueOf(breaker[1]) > oldest) {
+                oldest = Integer.valueOf(breaker[1]);
+            }
+
+        }
+
+        System.out.println("Age of the oldest: " + oldest);
 
     }
 }
--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -5,7 +5,29 @@ public class NameOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        
+        int oldest = 0;
+        String name = "";
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            
+            String[] breaker = input.split(",");
+            
+            if (Integer.valueOf(breaker[1]) > oldest) {
+                oldest = Integer.valueOf(breaker[1]);
+                name = breaker[0];
+            } 
+            
+        }
+        
+        System.out.println("Name of the oldest: " + name);
+        
+        
+        
     }
 }
